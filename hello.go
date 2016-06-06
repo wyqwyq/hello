739b3a7a608ffb3049a3af9382f9d5b0ace7e945
Yuri <yuri@Yuris-MacBook-Pro.local>
2016-06-06 20:57:21 +0800

    update

--- hello.go
+++ hello.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
+    s := [] int {1, 2, 3}
+    arr := s[:len(s) - 1]
+    if &s[0] == &arr[0]{
+        fmt.Printf("Only Ref\n")
+    }else{
+        fmt.Printf("Copy value")
+    }
     fmt.Printf("hello, world\n")
 }
